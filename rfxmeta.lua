c52d92ed66c09c8a30502b50a21ab0b8c7275667
java3east <florianbarth24>
2025-08-07 14:04:58 +0200

    :sparkles: add simulation and environment functions; enhance Test class with assertion methods

--- rfxmeta.lua
+++ rfxmeta.lua
@@ -13,9 +13,51 @@ function PACK(path, output, ignore) end
 function SIMULATION_CREATE(context) end
 
 ---@param sim number
+---@return number server
+function SIMULATION_GET_SERVER(sim) end
+
+---@param sim number
 ---@param path string
 ---@return number resource
 function RESOURCE_LOAD(sim, path) end
 
 ---@param resource number
 function RESOURCE_START(resource) end
+
+---@param server number
+---@param resource number
+---@return number environment
+function ENVIRONMENT_GET(server, resource) end
+
+---@param environment number
+---@param key string
+---@return any value
+function ENVIRONMENT_GET_VAR(environment, key) end
+
+---@class Test
+Test = {}
+
+---@param name string
+---@param cb fun(self: Test) : boolean
+function Test.new(name, cb) end
+
+---@param condition boolean
+---@param message string
+---@return boolean passed
+function Test.assert(condition, message) end
+
+---@param a any
+---@param b any
+---@param message string
+---@return boolean passed
+function Test.assertEqual(a, b, message) end
+
+---@param value any
+---@param acceptedValues any[]
+---@param message string
+---@return boolean passed
+function Test.assertOneOf(value, acceptedValues, message) end
+
+function Test.runAll() end
+
+function Test:run() end
